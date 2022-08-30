99830e7abb7433efcbe723144938386537d66759
Alexander Rengkat <successinmotion11@gmail.com>
2022-08-31 00:21:59 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const colors = {
+  brand: {
+    bg: "#141850",
+  },
+};
+const theme = extendTheme({ colors });
+root.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
